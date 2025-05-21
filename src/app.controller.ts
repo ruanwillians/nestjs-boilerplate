c861bd9b80ae578cb1fd9d2ccb9e722e260cdef0
Ruan Willians Mira Nogueira <108192932+ruanwillians@users.noreply.github.com>
2025-05-20 22:12:27 -0300

    setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ConfigService } from "@nestjs/config";
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly configService: ConfigService,
-  ) { }
+  ) {}
 
   @Get()
   getHello(): string {
